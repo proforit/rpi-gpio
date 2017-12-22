efc24404bb6786856bda7e1d3e35f6e25e750433
Kevin Buller <github@proforit.com>
2017-12-21 21:57:43 -0600

    creating variable structure

---
+++ variables.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# General variables
+logfile=/root/pinlogger.log
+
+# Pin names
+pin1="pin1"
+pin2="pin2"
+pin3="pin3"
+pin4="pin4"
+pin5="pin5"
+pin6="pin6"
+pin7="pin7"
+pin8="pin8"
+pin9="pin9"
+pin10="pin10"
+pin11="pin11"
+pin12="pin12"
+pin13="pin13"
+pin14="pin14"
+pin15="pin15"
+pin16="pin16"
+pin17="pin17"
+pin18="pin18"
+pin19="pin19"
+pin20="pin20"
+pin21="pin21"
+pin22="pin22"
+pin23="pin23"
+pin24="pin24"
+pin25="pin25"
+pin26="pin26"
+pin27="pin27"
+pin28="pin28"
+pin29="pin29"
+pin30="pin30"
+pin31="pin31"
+pin32="pin32"
+pin33="pin33"
+pin34="pin34"
+pin35="pin35"
+pin36="pin36"
+pin37="pin37"
+pin38="pin38"
+pin39="pin39"
+pin40="pin40"
+
+# Update/change variables in this file
+. ./variables-override.sh
