6e22dbb8455867dceed76a29710b5e19dd3e1292
Kevin Buller <github@proforit.com>
2017-12-21 22:04:38 -0600

    functions update

--- functions.sh
+++ functions.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Import variables
+. ./variables.sh
+
 # Prints the status of one, all or a range of pins.
 # Usage
 #  pincheck
 #  pincheck 35
 #  pincheck 30 40
-pincheck() {
+get-pin-status() {
   if [ $1 ] && [ $2 ]; then
     pinlow=$1
     pinhigh=$2
@@ -23,4 +26,31 @@ pincheck() {
 
   # Return a new line to be nice
   echo
+}
+
+# Prints time nicely.
+# From https://unix.stackexchange.com/questions/27013/displaying-seconds-as-days-hours-mins-seconds
+displaytime() {
+  local T=$1
+  local D=$((T/60/60/24))
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  (( $D > 0 )) && printf '%d days ' $D
+  (( $H > 0 )) && printf '%d hours ' $H
+  (( $M > 0 )) && printf '%d minutes ' $M
+  (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+  printf '%d seconds\n' $S
+}
+
+# Returns the current date as an epoch or if an epoch is passed to it,
+# it compares it to the current date and gives the difference.
+epoch() {
+  # If there is an arg, assume it's an epoch and compare it to now
+  if [ $1 ]; then
+    displaytime $(( $(epoch) - $( date --date=@$1 +'%s' ) ))
+  else
+  # If there isn't an arg, get the current date formatted as an epoch
+    date +'%s'
+  fi
 }
